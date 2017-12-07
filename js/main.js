1583e7156b1c5ebfcc4e4b86347c618f28b91ac7
Michał Niemiec <mniemiec@trans.eu>
2017-12-07 08:51:29 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var addNumbers = function (number1, number2) {
+    if (number1 && number2) {
+        return number1 + number2;
+    }
+};
+
+document.getElementById('calcBtn').addEventListener('click', function () {
+    var number1 = parseFloat(document.getElementById("number1").value);
+    var number2 = parseFloat(document.getElementById("number2").value);
+    var result = addNumbers(number1, number2);
+    document.getElementById("result").innerHTML = result;
+});
