0d5d475e9275d27a3474ecd79feff90ff04fe7b5
Michał Niemiec <michalniemiec28@gmail.com>
2017-12-11 23:08:23 +0100

    kurs2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-var addNumbers = function (number1, number2) {
-    return number1 + number2;
-};
+//         CZĘŚĆ 1
 
-document.getElementById('calcBtn').addEventListener('click', function () {
-    var number1 = parseFloat(document.getElementById("number1").value);
-    var number2 = parseFloat(document.getElementById("number2").value);
-    var result = addNumbers(number1, number2);
-    document.getElementById("result").innerHTML = result;
-});
+// var addNumbers = function (number1, number2) {
+//     return number1 + number2;
+// };
+
+// document.getElementById('calcBtn').addEventListener('click', function () {
+//     var number1 = parseFloat(document.getElementById("number1").value);
+//     var number2 = parseFloat(document.getElementById("number2").value);
+//     var result = addNumbers(number1, number2);
+//     document.getElementById("result").innerHTML = result;
+// });
+
+window.onload = function() {
+    // kod aplikacji
+};
