968cda3e7bfb6c1a9e91203f14f8fdd5119d37a3
Michał Niemiec <mniemiec@trans.eu>
2018-01-12 13:19:13 +0100

    uzupełnij funkcję onload

--- js/main.js
+++ js/main.js
@@ -1,54 +1,75 @@
-window.onload = function () {
-    var numbers = document.querySelectorAll(".num");
-    var operators = document.querySelectorAll(".operator");
-    var equal = document.querySelector(".equal");
-    var zero = document.querySelector(".zero");
-    var dot = document.querySelector(".dot");
-    var result = document.querySelector("#result");
-    var remove = document.querySelector("#delete");
-    var activeResult = false;
+class Kalkulator {
+    constructor(asdf, fdsa) {
+        this.numbers = document.querySelectorAll(".num");
+        this.operators = document.querySelectorAll(".operator");
+        this.equal = document.querySelector(".equal");
+        this.zero = document.querySelector(".zero");
+        this.dot = document.querySelector(".dot");
+        this.result = document.querySelector("#result");
+        this.remove = document.querySelector("#delete");
+        this.activeResult = false;
 
-    numbers.forEach(function (button) {
-        button.addEventListener("click", function () {
-            if (activeResult) {
-                result.value = button.value;
-                activeResult = false;
-            } else {
-                result.value += button.value;
-            }
-        });
-    });
+        this.onload();
+    }
 
-    operators.forEach(function (operator) {
-        operator.addEventListener("click", function () {
-            if (result.value !== "") {
-                result.value += operator.value;
-            }
-        })
-    });
+    onload() {
+        // ...
+    }
+}
 
-    zero.addEventListener("click", function () {
-        if (result.value !== '0') {
-            result.value += zero.value;
-        }
-    });
+window.onload = new Kalkulator();
 
-    dot.addEventListener("click", function () {
-        if (result.value === "") {
-            result.value = "0.";
-        } else {
-            result.value += ".";
-        }
-    });
+// window.onload = function () {
+//     var numbers = document.querySelectorAll(".num");
+//     var operators = document.querySelectorAll(".operator");
+//     var equal = document.querySelector(".equal");
+//     var zero = document.querySelector(".zero");
+//     var dot = document.querySelector(".dot");
+//     var { value: resultValue } = document.querySelector("#result");
+//     var remove = document.querySelector("#delete");
+//     var activeResult = false;
 
-    equal.addEventListener("click", function () {
-        if (result.value !== "") {
-            result.value = eval(result.value);
-            activeResult = true;
-        }
-    });
+//     numbers.forEach(button => {
+//         button.addEventListener("click", () => {
+//             if (activeResult) {
+//                 resultValue = button.value;
+//                 activeResult = false;
+//             } else {
+//                 result.value += button.value;
+//             }
+//         });
+//     });
 
-    remove.addEventListener("click", function () {
-        result.value = "";
-    });
-};
+//     operators.forEach(function (operator) {
+//         operator.addEventListener("click", function () {
+//             if (result.value !== "") {
+//                 result.value += operator.value;
+//             }
+//         })
+//     });
+
+//     zero.addEventListener("click", function () {
+//         if (result.value !== '0') {
+//             result.value += zero.value;
+//         }
+//     });
+
+//     dot.addEventListener("click", function () {
+//         if (result.value === "") {
+//             result.value = "0.";
+//         } else {
+//             result.value += ".";
+//         }
+//     });
+
+//     equal.addEventListener("click", function () {
+//         if (result.value !== "") {
+//             result.value = eval(result.value);
+//             activeResult = true;
+//         }
+//     });
+
+//     remove.addEventListener("click", function () {
+//         result.value = "";
+//     });
+// };
